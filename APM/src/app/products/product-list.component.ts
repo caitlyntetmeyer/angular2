ab8c0897592b9c021c4fb227187cf458d1233e87
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-08-04 14:20:32 -0400

    Added product list component

---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// Component decorator that defines our metadata:
+@Component({
+    selector: 'pm-products',
+    // Template defining our view:
+    templateUrl: 'app/products/product-list.component.html'
+})
+
+export class ProductListComponent{
+
+}
